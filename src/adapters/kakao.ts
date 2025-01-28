b3bf7c40fa87b44f512922ffe41c5ad537787273
minjeongHEO <hmjoung9770@gmail.com>
2025-01-28 16:54:14 +0900

    feat: add kakao maps adapter

---
+++ src/adapters/kakao.ts
@@ -0,0 +1,13 @@
+import { RoutePoint } from "../types";
+
+export interface KakaoLatLng {
+  getLat(): number;
+  getLng(): number;
+}
+
+export const fromKakaoLatLng = (path: KakaoLatLng[]): RoutePoint[] => {
+  return path.map((point) => ({
+    latitude: point.getLat(),
+    longitude: point.getLng(),
+  }));
+};
